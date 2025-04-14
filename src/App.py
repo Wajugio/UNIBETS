217ccb4b64f91a387f1923d41af9835e86a65d48
Wajugio <gderi005@fiu.edu>
2025-04-14 00:39:43 -0400

    Update App.py

--- src/App.py
+++ src/App.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import Flask, render_template, request
 from logic.betting_logic import calculate_odds
 from data.database import get_match_list
